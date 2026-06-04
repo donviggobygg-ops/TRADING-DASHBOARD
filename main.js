8e259625aee9a4ceae56204fc16006978de76eaa
donviggobygg-ops <donviggobygg@gmail.com>
2026-06-04 14:24:56 +0200

    MY TRADING DASHBOARD
    THAT I LOG TRADERS

---
+++ main.js
@@ -0,0 +1,29 @@
+// Import libraries
+import Chart from 'chart.js/auto'
+import * as LightweightCharts from 'lightweight-charts'
+
+// Expose to global scope for inline scripts
+window.Chart = Chart
+window.LightweightCharts = LightweightCharts
+
+console.log('✓ Trading Dashboard initialized')
+console.log('✓ Chart.js loaded')
+console.log('✓ lightweight-charts loaded')
+
+import { createChart, LineSeries } from 'lightweight-charts';
+
+const chart = createChart(document.body, { width: 400, height: 300 });
+const lineSeries = chart.addSeries(LineSeries);
+lineSeries.setData([
+    { time: '2019-04-11', value: 80.01 },
+    { time: '2019-04-12', value: 96.63 },
+    { time: '2019-04-13', value: 76.64 },
+    { time: '2019-04-14', value: 81.89 },
+    { time: '2019-04-15', value: 74.43 },
+    { time: '2019-04-16', value: 80.01 },
+    { time: '2019-04-17', value: 96.63 },
+    { time: '2019-04-18', value: 76.64 },
+    { time: '2019-04-19', value: 81.89 },
+    { time: '2019-04-20', value: 74.43 },
+]);
+
